58890a6ecabfda9837495c4f91825bec69471af8
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-10-23 01:42:35 +0200

    Finished basic bottom nav bar

---
+++ fluttershare/lib/pages/home.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttershare/pages/activity_feed.dart';
+import 'package:fluttershare/pages/profile.dart';
+import 'package:fluttershare/pages/search.dart';
+import 'package:fluttershare/pages/timeline.dart';
+import 'package:fluttershare/pages/upload.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:image/image.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isAuth = false;
+  // Page controller needed for bottom navigation bar
+  PageController pageController;
+  int pageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Page controller initialized in initState
+    pageController = PageController();
+    // Detect user sign in
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignIn(account);
+    }, onError: (error) {
+      print('Error signing in: $error');
+    });
+
+    // Reauthenticate user when app is opened
+    googleSignIn.signInSilently(suppressErrors: false).then((account) {
+      handleSignIn(account);
+    }).catchError((error) {
+      print('Error signing in: $error');
+    });
+  }
+
+  @override
+  void dispose() {
+    // Always dispose of pageController
+    pageController.dispose();
+    super.dispose();
+  }
+
+  // Auxiliary method to set auth boolean according to account obtained from sign in
+  handleSignIn(GoogleSignInAccount account) {
+    if (account != null) {
+      print('User signed in: $account');
+      setState(() {
+        isAuth = true;
+      });
+    } else {
+      setState(() {
+        isAuth = false;
+      });
+    }
+  }
+
+  void login() {
+    googleSignIn.signIn();
+  }
+
+  void logout() {
+    googleSignIn.signOut();
+  }
+
+  onPageChanged(int pageIndex) {
+    setState(() {
+      this.pageIndex = pageIndex;
+    });
+  }
+
+  onTap(int pageIndex) {
+    pageController.jumpToPage(pageIndex);
+  }
+
+  Widget buildAuthScreen() {
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[
+          Timeline(),
+          ActivityFeed(),
+          Upload(),
+          Search(),
+          Profile(),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: pageIndex,
+        onTap: onTap,
+        activeColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.whatshot),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_active),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo_camera, size: 35.0),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Scaffold buildUnauthScreen() {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Theme.of(context).accentColor,
+              Theme.of(context).primaryColor,
+            ])),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'FlutterShare',
+              style: TextStyle(
+                fontFamily: 'Signatra',
+                fontSize: 90.0,
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              onTap: login,
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/google_signin_button.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Showing appropriate screen depending on auth status
+    return isAuth ? buildAuthScreen() : buildUnauthScreen();
+  }
+}
