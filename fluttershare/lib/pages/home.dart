4f02d22b793bc5d8e7ce7c53390f07edc82ebb1b
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-10-23 15:24:42 +0200

    Create account screen done

--- fluttershare/lib/pages/home.dart
+++ fluttershare/lib/pages/home.dart
@@ -1,14 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttershare/models/user.dart';
 import 'package:fluttershare/pages/activity_feed.dart';
+import 'package:fluttershare/pages/create_account.dart';
 import 'package:fluttershare/pages/profile.dart';
 import 'package:fluttershare/pages/search.dart';
 import 'package:fluttershare/pages/timeline.dart';
 import 'package:fluttershare/pages/upload.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:image/image.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
+final usersRef = Firestore.instance.collection('users');
+final DateTime timestamp = DateTime.now();
+User currentUser;
 
 class Home extends StatefulWidget {
   @override
@@ -51,7 +56,7 @@ class _HomeState extends State<Home> {
   // Auxiliary method to set auth boolean according to account obtained from sign in
   handleSignIn(GoogleSignInAccount account) {
     if (account != null) {
-      print('User signed in: $account');
+      createUserInFirestore();
       setState(() {
         isAuth = true;
       });
@@ -60,6 +65,35 @@ class _HomeState extends State<Home> {
         isAuth = false;
       });
     }
+  }
+
+  void createUserInFirestore() async {
+    // 1) Check if user exists in users collection in database (according to their id)
+    final GoogleSignInAccount user = googleSignIn.currentUser;
+    DocumentSnapshot doc = await usersRef.document(user.id).get();
+
+    if (!doc.exists) {
+      // 2) If the user doesn't exist, take them to the create account page
+      final username = await Navigator.push(
+          context, MaterialPageRoute(builder: (context) => CreateAccount()));
+
+      // 3) Get username from create account, use it to make new user document in users collection
+      usersRef.document(user.id).setData({
+        'id': user.id,
+        'username': username,
+        'photoUrl': user.photoUrl,
+        'email': user.email,
+        'displayName': user.displayName,
+        'bio': '',
+        'timestamp': timestamp,
+      });
+
+      doc = await usersRef.document(user.id).get();
+    }
+
+    currentUser = User.fromDocument(doc);
+    print(currentUser);
+    print(currentUser.username);
   }
 
   void login() {
@@ -88,7 +122,11 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: PageView(
         children: <Widget>[
-          Timeline(),
+          //Timeline(),
+          RaisedButton(
+            child: Text('Logout'),
+            onPressed: logout,
+          ),
           ActivityFeed(),
           Upload(),
           Search(),
--- fluttershare/lib/pages/timeline.dart
+++ fluttershare/lib/pages/timeline.dart
@@ -1,9 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
-import 'package:fluttershare/widgets/progress.dart';
-
-final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -12,26 +8,10 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
-  void initState() {
-    //getUsers();
-    super.initState();
-  }
-
-  // void getUsers() {
-  //   usersRef.getDocuments().then((QuerySnapshot snapshot) {
-  //     snapshot.documents.forEach((DocumentSnapshot doc) {
-  //       print(doc.data);
-  //       print(doc.documentID);
-  //       print(doc.exists);
-  //     });
-  //   });
-  // }
-
-  @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: linearProgress(),
+      body: Text('Timeline'),
     );
   }
 }
